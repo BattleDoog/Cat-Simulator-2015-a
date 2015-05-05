707f51041d87700e3be09f7660c0398ec6b090ca
Austin Soohoo <kimcheetos@gmail.com>
2015-05-04 22:56:46 -0700

    comments

--- src/Play.cpp
+++ src/Play.cpp
@@ -11,11 +11,13 @@ void Play::catEntertain()
 		std::cout << "Choose your toy: " << std::endl;
 		std::getline(std::cin, phrase);
 
+		//based on toy, a value will be added to happiness
+
 		int x = withoutPlay;
 		int y;
-		y = 2 ^ (x/3);
+		y = 2 ^ (x/3); //is this the incrementaion? what's the purpose?
 	}	
-	else if(phrase == "play" && disapointValue = 100)
+	else if(phrase == "play" && disapointValue == 100)
 	{
 		std::cout <<  "Stop playing with your Cat!" std::endl;
 	}
